8cee337cf30e8100976d307146d95630ec1897ed
cemil3535 <cemilozcan35@gmail.com>
2024-08-13 22:31:07 +0300

    Add project files.

---
+++ ClosingOfFourWeek/BaseMakine.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosingOfFourWeek
+{
+    //base class
+    public abstract class BaseMakine
+    {
+        // Property to identify
+        public DateTime ProductDate { get; set; }
+        public string SerialNumber { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string OperatingSystem { get; set; }
+
+        //  Define the BaseMakine constructor for ProductDat
+        public BaseMakine()
+        {
+            ProductDate = DateTime.Now;
+
+        }
+        // Define a virtual method that displays general information
+        public virtual void DisplayInfo()
+        {
+
+            Console.WriteLine($"Seri Numarsi: {SerialNumber} Urun Adi: {ProductName} Urun Aciklamasi: {ProductDescription}  Isletim Sistemi: {OperatingSystem} Uretim Tarihi: {ProductDate.ToShortDateString()}");
+        }
+        // abstract metod 
+        public abstract void ProductNameGet();
+
+    }
+    // derived class
+    class Phone : BaseMakine
+    {
+        // TRLicensed of identify
+        public bool TRLicensed { get; set; }
+
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Turkiye Lisansi Var Mi?: {(TRLicensed ? "Evet" : "Hayir")}");
+        }
+
+        // abstract metod override
+        public override void ProductNameGet()
+        {
+            Console.WriteLine($"Telefonunuzun Adi-------> {ProductName}");
+        }
+    }
+
+    //derived class
+    class Computer : BaseMakine
+    {
+        // UsbGetNumber property of identify
+        int _usbGateNumber;
+        public int UsbGateNumber
+        {
+            get
+            {
+                return _usbGateNumber;
+            }
+
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _usbGateNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("USB giris sayisi 2 veya 4 olabilir.");
+                    _usbGateNumber = -1;
+                }
+            }
+        }
+
+        // Bluetooth property od identify
+        public bool Bluetooth { get; set; }
+
+        public override void DisplayInfo()
+        {
+            base .DisplayInfo();
+            Console.WriteLine($"USB Giris Sayisi: {UsbGateNumber}\nBluetooth Var Mi?: {(Bluetooth ? "Evet" : "Hayir")}");
+
+        }
+
+        // abstract metod override
+        public override void ProductNameGet()
+        {
+            Console.WriteLine($"Bilgisayarinizin Adi------> {ProductName}");
+        }
+    }
+
+
+}
---
+++ ClosingOfFourWeek/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClosingOfFourWeek;
+using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool repeat = true;
+
+    GoHere:
+        while (repeat)
+        {
+
+            Console.WriteLine("Telefon Uretmek icin 1\nBilgisayar uretmek icin 2'ye basiniz.");
+            string userEnter = Console.ReadLine();
+
+            //When 1 is selected, the phone generation is generated and the necessary values ​​are defined.
+            if (userEnter == "1")
+            {
+                Phone phone = new Phone();
+                Console.Write("Lutfen Seri Numarasini Giriniz: ");
+                phone.SerialNumber = Console.ReadLine();
+                Console.Write("Lutfen Telefonun Adini Giriniz: ");
+                phone.ProductName = Console.ReadLine();
+                Console.Write("Lutfen Aciklama Giriniz: ");
+                phone.ProductDescription = Console.ReadLine();
+                Console.Write("Lutfen Isletim Sistemini Giriniz: ");
+                phone.OperatingSystem = Console.ReadLine();
+                Console.Write("Turkiye Lisansli Mi? (Evet/Hayir): ");
+                phone.TRLicensed = Convert.ToBoolean(Console.ReadLine().ToLower() == "evet");
+
+                //Calling and displaying the defined values ​​in the relevant method
+                Console.WriteLine("Asagidaki Urun Basariyla Uretildi.");
+                Console.WriteLine("--------------------------------------------");
+                phone.DisplayInfo();
+            }
+            // When 2 is selected, the computer generation is generated and the necessary values ​​are defined.
+            else if (userEnter == "2")
+            {
+                Computer computer = new Computer();
+                Console.Write("Lutfen Seri Numarasini Giriniz: ");
+                computer.SerialNumber = Console.ReadLine();
+                Console.Write("Lutfen Bilgisayarinizin Adini Giriniz: ");
+                computer.ProductName = Console.ReadLine();
+                Console.Write("Lutfen Aciklama Giriniz: ");
+                computer.ProductDescription = Console.ReadLine();
+                Console.Write("Lutfen Isletim Sistemini Giriniz: ");
+                computer.OperatingSystem = Console.ReadLine();
+                Console.Write("Lutfen USB Giris Sayisini Giriniz: ");
+                computer.UsbGateNumber = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Bluetooth Var Mi? (Evet/Hayir): ");
+                computer.Bluetooth = Convert.ToBoolean(Console.ReadLine().ToLower() == "evet");
+
+
+                // Calling and displaying the defined values ​​in the relevant method
+                Console.WriteLine("Asagidaki Urun Basariyla Uretildi.");
+                Console.WriteLine("--------------------------------------------");
+                computer.DisplayInfo();
+            }
+            else
+            {
+                Console.WriteLine("Yanlis sayi girdiniz.");
+                goto GoHere;
+            }
+            //query whether the loop should continue
+            Console.WriteLine("Baska Bir Urun Uretmek Ister misiniz? (Evet/Hayir): ");
+            repeat = Console.ReadLine().ToLower() == "evet";
+        }
+        Console.WriteLine("Iyi Gunler!");
+    }
+
+}
+
+
+
+
+
+
+
